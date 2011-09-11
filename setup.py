5b68e815c5a721beb22b87a74ca22ae4c43f04b1
Tycho Andersen <tycho@tycho.ws>
2011-09-11 11:04:58 -0500

    Add distutils support for tyconfig.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(py_modules = ['tyconfig'],
+      name = 'tyconfig',
+      version = '0.1',
+      description = 'Improvement on ConfigParser',
+      author = 'Tycho Andersen',
+      author_email = 'tycho@tycho.ws',
+      url = 'http://stackoverflow.com/questions/2140985/setup-a-git-external-for-remote-repo',
+     )
